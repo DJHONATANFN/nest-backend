2c50dcb28ce65db926753e4bc4661a4e88d4d752
Jhonatan <jfloresn1507@gmail.com>
2023-09-28 19:50:18 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Backend nest
+
+```
+docker compose up -d
+
+```
+
+Copiar en ```env.template``` y renombralo a ```.env```
---
+++ src/auth/dto/index.ts
@@ -0,0 +1,5 @@
+
+export * from './create-user.dto';
+export * from './login';
+export * from './register-user.dto';
+export * from './update-auth.dto';
